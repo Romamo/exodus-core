1d6d553db41b62ee22c8ee9fb5354d42a6860502
Romamo <romamo@yandex.ru>
2024-05-23 13:28:30 +0300

    Lowered version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
     'cryptography==38.0.1',
     'dhash==1.4',
     'jellyfish==0.5.6',
-    'Pillow==10.3.0',
+    'Pillow==9.5.0',
     'requests>=2.23,<2.32',
     'six==1.15.0',
     'traitlets==4.3.2'
